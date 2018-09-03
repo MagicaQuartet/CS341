77c13f27e53d7e3b093a441e8f5100d3df318cdf
MagicaQuartet <dev.dskim@gmail.com>
2018-09-03 20:15:35 +0900

    set basic structure of client and server

--- Project#0/client.c
+++ Project#0/client.c
@@ -1,19 +1,66 @@
+/* client.c */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define MAXDATASIZE 1000
 
 int main (int argc, char *argv[]) {
 	int clientfd;
-	char *host, *port, *operation, *shift;
+	struct addrinfo hints, *servinfo, *p;
+	char buf[MAXDATASIZE];
+	int gai, numbytes;
 	
 	if (argc != 9) {
 		fprintf(stderr, "usage: %s -h <host> -p <port> -o <operation> -s <shift>\n", argv[0]);
-		exit(0);
+		return argc;
+	}
+	
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((gai = getaddrinfo(argv[2], argv[4], &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(gai));
+		return 1;
 	}
 
-	host = argv[2];
-	port = argv[4];
-	operation = argv[6];
-	shift = argv[8];
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			continue;
+		}
 
-	exit(0);	
+		if (connect(clientfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(clientfd);
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo);
+
+	if ((numbytes = recv(clientfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		perror("recv");
+		exit(1);
+	}
+
+	buf[numbytes] = '\0';
+
+	printf("client: received '%s'\n", buf);
+
+	close(clientfd);
+
+	return 0;	
 }
--- Project#0/server.c
+++ Project#0/server.c
@@ -1,11 +1,85 @@
+/* server.c */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define BACKLOG 10
+#define MAXDATASIZE 1000
 
 int main (int argc, char *argv[]) {
 	int listenfd, connfd;
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage clientaddr;
+	socklen_t clientlen;
+	int gai, yes=1;
 
 	if (argc != 3) {
 		fprintf(stderr, "usage: %s -p <port>\n", argv[0]);
-		exit(0);
+		return argc;
 	}
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if ((gai = getaddrinfo(NULL, argv[2], &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(gai));
+		return argc;
+	}
+
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			continue;
+		}
+
+		if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+
+		if (bind(listenfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(listenfd);
+			continue;
+		}
+
+		break;
+	}
+
+	freeaddrinfo(servinfo);
+
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(listenfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	while(1) {
+		clientlen = sizeof(struct sockaddr_storage);
+		connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+		if (connfd == -1) {
+			continue;
+		}
+
+		if (!fork()) {
+			if (send(connfd, "Hello, world!", 13, 0) != -1)
+				perror("sent");
+			close(connfd);
+			exit(0);
+		}
+		close(connfd);
+	}
+
+	return 0;
 }
